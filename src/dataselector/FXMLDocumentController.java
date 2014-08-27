009fd0aa441ccafe7044115f7b06d33aaee712e7
minihessel <Eskil@pc-116-58.eduroam.uib.no>
2014-08-27 11:24:35 +0200

    Lagt til mange endringer
    Mulighet for brukeren å velge hvilke kolonner han vil ha visualisert
    
    Fleksibel tabelluthenting. Nå henter programmet først ut alle tabeller,
    så velger brukeren hvilken tabell han vil ha. Deretter henter koden ut
    alle kolonner og rader for den tabellen.

--- src/dataselector/FXMLDocumentController.java
+++ src/dataselector/FXMLDocumentController.java
@@ -5,15 +5,20 @@
  */
 package dataselector;
 
-import dataselector.mouseHooverAnimationPieChart.MouseExitAnimation;
-import dataselector.mouseHooverAnimationPieChart.MouseHoverAnimation;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -23,17 +28,17 @@ import javafx.scene.Group;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TreeTableColumn.CellEditEvent;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
+import org.fxmisc.easybind.EasyBind;
 
 /**
  *
@@ -43,16 +48,29 @@ public class FXMLDocumentController implements Initializable {
 
     SQL_manager sql_manager = new SQL_manager();
 
-    static ObservableList<Record> dataen = FXCollections.observableArrayList();
+    ObservableList<List<String>> dataen = FXCollections.observableArrayList();
     static SortedList<Record> sortedData;
-    static ObservableList<PieChart.Data> dataList = FXCollections.observableArrayList();
+    List<TableColumn> lstClm = new ArrayList<TableColumn>();
+
     static XYChart.Series series1 = new XYChart.Series();
+    static ObservableList<String> rows = FXCollections.observableArrayList();
+
     @FXML
     protected Label caption;
     @FXML
     protected TextField filterField;
     @FXML
     TableView tableView;
+    @FXML
+    TextField t;
+    @FXML
+    TableView tableView2;
+    @FXML
+    ChoiceBox choiceBoxTable;
+    @FXML
+    ChoiceBox choiceBoxName;
+    @FXML
+    ChoiceBox choiceBoxValue;
     @FXML
     protected BarChart barChart;
     @FXML
@@ -77,8 +95,8 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     protected void handleButtonAction(ActionEvent event) throws ClassNotFoundException, SQLException {
-        sql_manager.connectToSql("localhost", 8889, "mysql");
-        filterData();
+        Connection conn = sql_manager.getConnection("localhost", 8889, "mysql");
+        getAllTables();
 
     }
 
@@ -100,13 +118,14 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     protected void handleButtonAction5(ActionEvent event) throws SQLException, ClassNotFoundException {
-        sql_manager.connectToSql(txtIPadress.getText(), Integer.parseInt(txtPortNumber.getText()), txtSQLinstance.getText());
+        Connection conn = sql_manager.getConnection(txtIPadress.getText(), Integer.parseInt(txtPortNumber.getText()), txtSQLinstance.getText());
+        getAllTables();
 
     }
 
     @FXML
     protected void handleButtonAction1(ActionEvent event) throws SQLException, ClassNotFoundException {
-        filterData();
+        //filterData();
     }
 
     @FXML
@@ -115,9 +134,41 @@ public class FXMLDocumentController implements Initializable {
         dataSelectGroup.visibleProperty().set(false);
         sqlConnectGroup.visibleProperty().set(false);
 
+        
         barChart.visibleProperty().set(false);
         pieChart.visibleProperty().set(true);
-        getPieChartData();
+   int userChosenDataNameColumnIndex = 0  ;
+int userChosenDataValueColumnIndex = 0 ;
+        Iterator itr =  lstClm.iterator();
+        int counter = 0; 
+        Boolean thereIsAValue = false; 
+        Boolean thereIsAName = false; 
+        while(itr.hasNext())
+        {
+          if (counter>1){
+              System.out.println("FEIL");
+          }
+          else{
+            if(isInteger(lstClm.get(counter).getCellData(0).toString()))
+                    {   thereIsAValue = true; 
+                        userChosenDataValueColumnIndex = Integer.parseInt(lstClm.get(counter).getId());
+                        
+                    }
+            else {
+                thereIsAName = true; 
+                userChosenDataNameColumnIndex = Integer.parseInt(lstClm.get(counter).getId());
+            }
+          }
+                    counter++;
+                       itr.next();
+        }
+        if(thereIsAName && thereIsAValue == true )
+        {
+            getPieChartData(userChosenDataValueColumnIndex,userChosenDataNameColumnIndex);
+        }
+        
+     
+      
     }
 
     @FXML
@@ -150,54 +201,191 @@ public class FXMLDocumentController implements Initializable {
         menu.getItems().add(item);
         tableView.setContextMenu(menu);
 
+        addListenerToChoiceBox(choiceBoxTable);
+
     }
 
-    protected void getPieChartData() {
-        pieChart.getData().clear();
-        for (Record record : sortedData) {
-            dataList.add(new PieChart.Data(record.getFieldMonth(), record.getFieldValue()));
+    private void addListenerToChoiceBox(ChoiceBox choicebox) {
+        //adding a listener to the coicebox to pull out the choice the user selected
+        choicebox.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                String newSelection = (String) newValue;
+                try {
+                    getDataForSelectedTable(newSelection);
+                } catch (SQLException ex) {
+
+                }
+                System.out.println("choiceBoxSelected by Listener: " + newSelection);
+            }
+        });
+    }
+
+    private void getAllTables() throws SQLException {
+        //Henter ut alle tabellene i databasen ved hjelp av metadata
+        DatabaseMetaData md = SQL_manager.conn.getMetaData();
+        ResultSet rs = md.getTables(null, null, "%", null);
+
+        while (rs.next()) {
+            //deretter legger jeg til alle tabellene i en choicebox så brukeren kan velge hvilken tabell
+            choiceBoxTable.getItems().add(rs.getString(3));
         }
-        pieChart.setData(dataList);
-        for (PieChart.Data d : dataList) {
-            d.getNode().setOnMouseEntered(new MouseHoverAnimation(d, pieChart));
-            d.getNode().setOnMouseExited(new MouseExitAnimation());
-            d.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent e) {
-                    caption.setText("Verdi for valgt pai: " + String.valueOf(d.getPieValue()));
+
+    }
+
+    private void getDataForSelectedTable(String tableName) throws SQLException {
+        // henter ut all data for en spesifikk tabell.
+        tableView.getColumns().removeAll(tableView.getColumns());
+
+        String SQL = "SELECT * FROM " + tableName;
+        ResultSet rs = sql_manager.getDataFromSQL("localhost", 8889, "mysql", SQL);
+
+        //først henter vi ut alle kolonnene og legger til de i tableview
+        for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+            //We are using non property style for making dynamic table
+            System.out.print("her kommern " + i);
+            final int j = i;
+            TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+            System.out.println(col);
+            col.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                public ObservableValue<String> call(TableColumn.CellDataFeatures<ObservableList, String> param) {
+                    return new SimpleStringProperty(param.getValue().get(j).toString());
                 }
             });
+            
+            col.setId((String.valueOf(i)));
+            col.prefWidthProperty().bind(tableView.widthProperty().divide(4)); //for å automatisere bredden på kolonnene 
+            //col.setGraphic(new CheckBox());
+            tableView.getColumns().addAll(col);
+            addCheckBoxesToColumns(col);
+
         }
+
+        //deretter legges all dataen til i kolonnene ved hjelp av rader
+        while (rs.next()) {
+            ObservableList<String> row = FXCollections.observableArrayList();
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                if (rs.getString(i) != null) {
+                    row.add(rs.getString(i));
+                    rows.add(rs.getString(i));
+
+                    System.out.println(rs.getString(i));
+                } else {
+                    row.add(" ");
+                }
+
+            }
+            dataen.add(row);
+        }
+
+        tableView.setItems(dataen);
+
+    }
+    
+    public static boolean isInteger(String str) {
+	if (str == null) {
+		return false;
+	}
+	int length = str.length();
+	if (length == 0) {
+		return false;
+	}
+	int i = 0;
+	if (str.charAt(0) == '-') {
+		if (length == 1) {
+			return false;
+		}
+		i = 1;
+	}
+	for (; i < length; i++) {
+		char c = str.charAt(i);
+		if (c <= '/' || c >= ':') {
+			return false;
+		}
+	}
+	return true;
+}
+
+    private void addCheckBoxesToColumns(TableColumn col) {
+        EventHandler<ActionEvent> handler = new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                CheckBox cb = (CheckBox) event.getSource();
+                TableColumn column = (TableColumn) cb.getUserData();
+                if (cb.isSelected()) {
+                    System.out.println(column);
+                    lstClm.add(column);
+                } else {
+                    lstClm.remove(column);
+                }
+
+            }
+        };
+        CheckBox cb = new CheckBox();
+        cb.setUserData(col);
+        cb.setOnAction(handler);
+        col.setGraphic(cb);
+
+    }
+
+    protected void getPieChartData(Integer userChosenDataValueColumnIndex, Integer userChosenDataNameColumnIndex) {
+        
+        
+        
+           System.out.println("Kolonne id for data er " + userChosenDataValueColumnIndex + "Kolonne id for name er " +userChosenDataNameColumnIndex);
+        ObservableList<PieChart.Data> pieChartData
+                = EasyBind.map(dataen, rowData -> {
+            String name = (String) rowData.get(userChosenDataNameColumnIndex);
+            int value = Integer.parseInt(rowData.get(userChosenDataValueColumnIndex));
+            return new PieChart.Data(name, value);
+        });
+       System.out.println("aa " + pieChartData.get(0));
+       
+        /* pieChart.setData(pieChartData);
+
+         for (PieChart.Data d : pieChartData) {
+         d.getNode().setOnMouseEntered(new MouseHoverAnimation(d, pieChart));
+         d.getNode().setOnMouseExited(new MouseExitAnimation());
+         d.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+         @Override
+         public void handle(MouseEvent e) {
+         caption.setText("Verdi for valgt pai: " + String.valueOf(d.getPieValue()));
+         }
+         });
+         }*/
     }
 
     protected void getBarChartData() {
+        Iterator itr = tableView.getItems().iterator();
+
         series1.getData().clear();
-        for (Record record : sortedData) {
-            series1.getData().add(new XYChart.Data(record.getFieldMonth(), record.getFieldValue()));
-        }
+
+        series1.getData().add(new XYChart.Data(itr.next(), dataen.get(1)));
+
         barChart.setData(FXCollections.observableArrayList(series1));
     }
 
-    private void filterData() {
-        FilteredList<Record> filteredData = new FilteredList<>(dataen, (Record p) -> true);
-        filteredData.setPredicate((Record record) -> {
-            // If filter text is empty, display all persons.
-            if (filterField.getText() == null || filterField.getText().isEmpty()) {
-                return true;
-            }
-            // Compare first name and last name of every person with filter text.
-            String lowerCaseFilter = filterField.getText().toLowerCase();
-            if (record.getFieldMonth().toLowerCase().indexOf(lowerCaseFilter) != -1) {
-                return true; // Filter matches first name.
-            }
-            return false; // Does not match.
-        });
-        // 3. Wrap the FilteredList in a SortedList.
-        sortedData = new SortedList<Record>(filteredData);
-        // 4. Bind the SortedList comparator to the TableView comparator.
-        sortedData.comparatorProperty().bind(tableView.comparatorProperty());
-        // 5. Add sorted (and filtered) data to the table.
-        tableView.setItems(sortedData);
-    }
-
+    /* private void filterData() {
+     FilteredList<Record> filteredData = new FilteredList<>(dataen, (Record p) -> true);
+     filteredData.setPredicate((Record record) -> {
+     // If filter text is empty, display all persons.
+     if (filterField.getText() == null || filterField.getText().isEmpty()) {
+     return true;
+     }
+     // Compare first name and last name of every person with filter text.
+     String lowerCaseFilter = filterField.getText().toLowerCase();
+     if (record.getFieldMonth().toLowerCase().indexOf(lowerCaseFilter) != -1) {
+     return true; // Filter matches first name.
+     }
+     return false; // Does not match.
+     });
+     // 3. Wrap the FilteredList in a SortedList.
+     sortedData = new SortedList<Record>(filteredData);
+     // 4. Bind the SortedList comparator to the TableView comparator.
+     sortedData.comparatorProperty().bind(tableView.comparatorProperty());
+     // 5. Add sorted (and filtered) data to the table.
+     tableView.setItems(sortedData);
+     }
+     */
 }
--- src/dataselector/SQL_manager.java
+++ src/dataselector/SQL_manager.java
@@ -6,42 +6,58 @@
 
 package dataselector;
 
-import static dataselector.FXMLDocumentController.dataen;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import snaq.db.DBPoolDataSource;
+import java.sql.Statement;
 
 /**
  *
  * @author Eskil
  */
 public class SQL_manager {
+     static Connection conn;
+
+    static Statement st;
 
     public SQL_manager () {
     
         
     }
     
-    public void bindSQLdata(String title, ResultSet rs) throws SQLException {
-        /* Etter at SQL er oppkoblet henter vi ut dataen fra databasen og binder den til en streng og en double
-        Deretter legger vi til strengen og doubleverdien til i dataListen
-         */
-        System.out.println("HER KOMMER DEN");
-        String test = rs.getString("asd");
-        // System.out.print(test);
-        Integer val = rs.getInt("val");
-        dataen.add(new Record(test, val));
-        System.out.println(dataen);
-    }
 
-    protected void connectToSql(String mySqlAdress, Integer myPort,String sqlInstance) throws SQLException{
+  
+   
+
+   protected Connection getConnection(String mySqlAdress, Integer myPort,String sqlInstance) throws SQLException{
+    
+        
+        try {
+            // Step 1: Load the JDBC driver.
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            System.out.println("Driver Loaded.");
+            // Step 2: Establish the connection to the database.
+              String myUrl = "jdbc:mysql://"+mySqlAdress+":"+myPort + "/" + sqlInstance;
+
+            conn = DriverManager.getConnection(myUrl, "root", "1234");
+            System.out.println("Got Connection.");
+
+            st = conn.createStatement();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            e.printStackTrace();
+
+        }
+        return conn;
+   }
+
+ 
+
+          protected ResultSet getDataFromSQL(String mySqlAdress, Integer myPort,String sqlInstance,String SQL) throws SQLException{
        
-      
-    dataen.clear();
-        System.out.print("Kobler til");
-      
         try {
             //String myDriver = "org.gjt.mm.mysql.Driver";
             
@@ -51,23 +67,30 @@ public class SQL_manager {
               + "user=root&password=1234");
             DriverManager.setLoginTimeout(2);
             //Connection conn = DriverManager.getConnection(myUrl, "root", "1234");
-            Connection conn = DriverManager.getConnection(myUrl+"?"
-              + "user=root&password=1234");
+          
             DriverManager.setLoginTimeout(2);
-             String SQL = "SELECT asd,val FROM test";
+             
             ResultSet rs = conn.createStatement().executeQuery(SQL);
-               while (rs.next()) {
-                /* Kjører metoden for å binde data til dataList(datasettet) */
-                System.out.println("faen");
-                bindSQLdata("asd", rs);
-
-            }
+              
+            return rs; 
         } 
       catch (Exception e)
 {
   System.err.println("Got an exception! ");
   System.err.println(e.getMessage());
 }
+        return null;
     }
-    
 }
+    
+    
+
+    
+    
+    
+    
+
+
+    
+    
+
